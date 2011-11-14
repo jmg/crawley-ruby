e64f6b07c637c9baf41d03202715863f05eed193
Juan Manuel García <jmg.utn@gmail.com>
2011-11-14 11:14:41 -0300

    It works

---
+++ examples/crawler_pypi.rb
@@ -0,0 +1,35 @@
+$LOAD_PATH << '../crawley'
+
+require 'crawlers'
+require 'scrapers'
+
+class PypiScraper < BaseScraper
+
+    @@matching_urls = ["%pypi.python.org/pypi%"]
+
+    def scrape response        
+        super response
+    end
+end
+
+class PypiCrawler < BaseCrawler
+
+    #add your starting urls here
+    @@start_urls = ["http://pypi.python.org/pypi"]
+
+    #add your scraper classes here
+    @@scrapers = [PypiScraper.new]
+
+    #specify your maximum crawling depth level
+    @@max_depth = 1
+
+end    
+
+def run_crawler
+    crawler = PypiCrawler.new true
+    crawler.start
+end
+
+if __FILE__ == $0
+    run_crawler
+end
