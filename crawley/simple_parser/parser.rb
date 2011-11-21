cfbab808f76c9d83e87170cc4c23169ad443e0fd
David Litvak <david.litvakb@gmail.com>
2011-11-21 15:08:16 -0300

    implemented almost all the dsl... still need to implement login and post

--- crawley/simple_parser/parser.rb
+++ crawley/simple_parser/parser.rb
@@ -5,7 +5,13 @@ require 'dm-migrations'
 @selectors_hash = Hash.new
 @scrapers_hash = Hash.new
 @crawlers = []
+@allowed_urls = []
+@black_list = []
 @max_depth = 0
+@max_concurrency_level = 25
+@requests_delay = 100
+@requests_deviation = 300
+@search_all_urls = true
 
 class Table
     include DataMapper::Resource 
@@ -31,10 +37,19 @@ class Scraper
 end
 
 class Crawler
-    def initialize scrapers, urls, max_depth
+    def initialize scrapers, urls, max_depth, 
+                   allowed_urls, black_list, 
+                   max_concurrency_level, requests_delay,
+                   requests_deviation, search_all_urls
         @scrapers = scrapers
         @urls = urls
         @max_depth = max_depth
+        @allowed_urls = allowed_urls
+        @black_list = black_list
+        @max_concurrency_level = max_concurrency_level
+        @requests_delay = requests_delay
+        @requests_deviation = requests_deviation
+        @search_all_urls = search_all_urls
     end
 
     def run
@@ -48,23 +63,34 @@ class Crawler
 
         DataMapper::Logger.new($stdout, :debug)
         DataMapper.setup(:default, 'sqlite::memory:')
-
         DataMapper.setup(:default, 'sqlite:///' + Dir.pwd + '/base.db')
-
         DataMapper.auto_migrate!
     end
 end
 
 def crawl urls, &table_block
     @crawlers = []
+    @allowed_urls = []
+    @black_list = []
+    @max_depth = 0
+    @max_concurrency_level = 25
+    @requests_delay = 100
+    @requests_deviation = 300
     @scrapers_hash = Hash.new
+    @search_all_urls = true
     table_block.call
     if urls.respond_to? :each
         urls.each do |an_url|
-            _add_crawlers an_url, @max_depth
+            _add_crawlers an_url, @max_depth, @allowed_urls, 
+                                  @black_list, @max_concurrency_level,
+                                  @requests_delay, @requests_deviation,
+                                  @search_all_urls
         end
     else
-        _add_crawlers urls, @max_depth
+        _add_crawlers urls, @max_depth, @allowed_urls, 
+                            @black_list, @max_concurrency_level,
+                            @requests_delay, @requests_deviation,
+                            @search_all_urls
     end
 
     @crawlers.each do |crawler|
@@ -72,12 +98,43 @@ def crawl urls, &table_block
     end
 end
 
-def _add_crawlers urls, max_depth
-    @crawlers.push Crawler.new @scrapers_hash.values, urls, max_depth
+def _add_crawlers urls, max_depth,
+                  allowed_urls, black_list, 
+                  max_concurrency_level, requests_delay,
+                  requests_deviation, search_all_urls
+    @crawlers.push Crawler.new @scrapers_hash.values, urls, 
+                               max_depth, allowed_urls,
+                               black_list, max_concurrency_level,
+                               requests_delay, requests_deviation,
+                               search_all_urls
 end
 
-def max_depth depth
+def max_depth depth=0
     @max_depth = depth
+end
+
+def max_concurrency_level level=25
+    @max_concurrency_level = level
+end
+
+def requests_delay miliseconds=100
+    @requests_delay = miliseconds
+end
+
+def requests_deviation miliseconds=300
+    @requests_deviation = miliseconds
+end
+
+def allowed_urls url_list=[]
+    @allowed_urls = url_list
+end
+
+def black_list url_list=[]
+    @black_list = url_list
+end
+
+def search_all_urls a_boolean=true
+    @search_all_urls = a_boolean
 end
 
 def table table_name, &fields_block
@@ -93,13 +150,14 @@ end
 if __FILE__ == $0
     crawl "http://pypi.python.org/pypi/cilantro/0.9b4" do
         max_depth 2
+        allowed_urls []
+        black_list []
+        max_concurrency_level 1000
+        requests_delay 1000
+        requests_deviation 1000
+        search_all_urls
 
         #TODO
-        #allowed_urls []
-        #black_list []
-        #max_concurrency_level 1000
-        #requests_delay 1000
-        #requests_deviation 1000
         #post_url "url" do
         #   param "nombre" do
         #       "value"
@@ -114,7 +172,6 @@ if __FILE__ == $0
         #   end
         #   ...
         #end
-        #search_all_urls true
 
         table "MI_TABLA" do
             field "MI_CAMPO_0" do
